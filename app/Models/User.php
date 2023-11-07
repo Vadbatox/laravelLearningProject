b8f567f3d60d513549fe6c3b11ada29c0f40c10d
Vad Batomene <batomenevad7@gmail.com>
2023-11-07 20:01:59 +0100

    Insertion avec Eloquent

--- app/Models/User.php
+++ app/Models/User.php
@@ -35,10 +35,11 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be cast.
-     *
+
      * @var array<string, string>
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
 }
---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+
+
+
+        User::create([
+            "name"=> "Vad Batomene",
+            "email"=> "batomenevad7@gmail.com",
+            "password"=> bcrypt("batox1205")]);
+        User::create( [
+            "name"=>"Yvanie",
+            "email"=> "yvanie@gmail.com",
+            "password"=> bcrypt("1234yva")
+        ]);
+
+
+        User::create([
+                "name"=>"Jean Mbiya",
+                "email"=> "Letecode@gmail.com",
+                "password"=> bcrypt("1234jcmbiya")
+            ]);
+
+        User::create( [
+                "name"=>"Rabin",
+                "email"=> "Rabin@gmail.com",
+                "password"=> bcrypt("1234jcmbiya")
+        ]);
+        User::create( [
+                "name"=>"Joseph Kabila",
+                "email"=> "kabila@gmail.com",
+                "password"=> bcrypt("1234Rais")
+            ]
+        );
+
+
+    }
+}
